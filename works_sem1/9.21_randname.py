7db0c86d4bbf50b8876243f294ff371b1af51066
Alejandro Alonso <66284316+AAWorks@users.noreply.github.com>
2021-09-21 10:39:10 -0400

    Update 9.21_randname.py

--- works_sem1/9.21_randname.py
+++ works_sem1/9.21_randname.py
@@ -2,3 +2,13 @@ import random
 
 def pickname_conc(pd1, pd2):
   return random.choice(pd1 + pd2)
+
+def pickname_norm(pd1, pd2):
+	def get_val(i):
+		if i > len(pd1):
+			return pd2[i % pd1]
+		else:
+			return pd1[i]
+			
+	rand_idx = random.randrange(len(pd1)+len(pd2))
+	return get_val(rand_idx)
