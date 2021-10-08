148afac06155e68a0c56a0b1ec615b937a6978f5
AAWorks <axalonso12@gmail.com>
2021-10-08 08:38:34 -0400

    first draft k13

--- hw_cw/13_tempwork/app.py
+++ hw_cw/13_tempwork/app.py
@@ -1,1 +1,17 @@
+# Clyde 'Thluffy' Sinclair
+# SoftDev
+# Oct 2021
 
+import occupations
+from flask import Flask, render_template
+app = Flask(__name__)
+
+coll = occupations.init()
+
+@app.route("/occupyflaskst")
+def test_tmplt():
+    return render_template( 'tablified.html', foo="Occupations", collection=coll)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
---
+++ hw_cw/13_tempwork/occupations.py
@@ -0,0 +1,14 @@
+import csv
+
+def init():
+    jobPercentages = {}
+    with open('data//occupations.csv') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            job = row["Job Class"]
+            percentage = float(row["Percentage"])
+            if job == 'Total':
+                totalPercentage = percentage
+            else:
+                jobPercentages[job] = float(percentage)
+    return jobPercentages
