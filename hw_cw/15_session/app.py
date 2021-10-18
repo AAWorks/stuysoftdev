bcc26390ef13bd355e0b20c3381704da23c7dd67
AAWorks <axalonso12@gmail.com>
2021-10-18 08:16:52 -0400

    folder created for k15

---
+++ hw_cw/15_session/app.py
@@ -0,0 +1,37 @@
+"""
+Waahoos: Alejandro Alonso, Ivan Mijacika, William Chen
+SoftDev
+K14 -- Form and Function
+2021-10-14
+"""
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/") #, methods=['GET', 'POST'])
+def disp_loginpage():
+    """
+    create the basic login page
+    """
+    return render_template( 'login.html')
+
+
+@app.route("/auth") # , methods=['GET', 'POST'])
+def authenticate():
+    """
+    generate and fill out the response page using flask vars
+    """
+    return render_template('response.html', username=request.args.get('username'))  #response to a form submission
+
+
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
