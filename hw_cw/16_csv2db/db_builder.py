902bc208a04ddd99b4309879007668f0df22a22a
AAWorks <axalonso12@gmail.com>
2021-10-24 15:10:10 -0400

    docstrings and diag print statements

--- hw_cw/16_csv2db/db_builder.py
+++ hw_cw/16_csv2db/db_builder.py
@@ -9,11 +9,16 @@ con = sqlite3.connect("discobandit.db") # change to 'sqlite:///your_filename.db'
 cur = con.cursor()
 
 def crt_table(file, t):
+    """
+    creates a sql table named t given a csv file with 3 columns in discobandit.db
+    """
     with open(file,'r') as fin:
         data = csv.DictReader(fin)
+        #print("***DIAG: dictreader obj***")
         headers = data.fieldnames
         headers_str = "(" + ", ".join(data.fieldnames) + ")"
         vals = [(row[headers[0]], row[headers[1]], row[headers[2]]) for row in data] #transferring dictreader values to a 2d array
+        #print("***DIAG: vals***")
 
     cur.execute(f"CREATE TABLE {t} {headers_str};") # creates table
     cur.executemany(f"INSERT INTO {t} {headers_str} VALUES (?, ?, ?);", vals) #for row of values in list object vals, inserts the values into the table
